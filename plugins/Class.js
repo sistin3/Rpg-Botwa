88f67a1cebb8a6a43fb2060b95c391357dc7147d
nobuyaki <72033395+Nobuyaki@users.noreply.github.com>
2021-05-21 22:06:51 +0700

    Update and rename Classes.js to Class.js

---
+++ plugins/Class.js
@@ -0,0 +1,18 @@
+//Being Created
+
+let handler = async (m, { conn, usedPrefix, command, text, args }) => {
+
+  let player = global.DATABASE._data.users[m.sender]
+  let pick = (args[0] || '').toLowerCase()
+  switch (class) {
+      case 'tank':
+        player.class = tank
+        player.attack = player.attack + 5
+        player.defense = player.defense + 15
+        break
+      case 'assasins':
+      case 'asasin':
+        player.class = assasins
+        player.attack = player.attack + 12
+        player.defense = player.defense + 3
+        break
